7b1b6824a60ee5576e3cfbafeb983b8f4876927f
MJoe111 <joemoussally111@gmail.com>
2022-05-20 16:36:23 +0300

    Adding turn based rounds

--- js/game.js
+++ js/game.js
@@ -2,6 +2,8 @@ const red = document.getElementById("red");
 const green = document.getElementById("green");
 const blue = document.getElementById("blue");
 const yellow = document.getElementById("yellow");
+var playerTurn = false;
+var round = 1;
 
 buttonsArray = document.getElementsByClassName("button")
 
@@ -22,17 +24,24 @@ const getButton = () => {
     return buttons[parseInt(Math.random()*buttons.length)]
 }
 
+//function for clicked button
 const buttonClick = (button) => {
-    console.log(button)
+    if (playerTurn) { //player can't choose untill the turn begins
+        // console.log(button)
+    }
+    
 }
 
 const maze = async () => {
+
     for (let button of order) {
         active(button);
         await new Promise(r => setTimeout(r, 1000));
     }
+
+    //player's turn to play
+    playerTurn = true;
 }
 
-const order = [getButton(),getButton(),getButton(),getButton()];
-
+const order = [getButton()];
 maze()
